a8ad57b7a583aadd4a5532392df1da94c010a08a
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-13 18:30:34 +0100

    initial commit

---
+++ src/middlewares/require-auth.ts
@@ -0,0 +1,13 @@
+import { Response, NextFunction } from 'express';
+// import User from '../../src/models/user';
+import { NotAuthorizedError } from '../errors/not-authorized-error'
+
+export const requireAuth = async (req: Req, res: Response, next: NextFunction) => {
+    if(!req.currentUser) throw new NotAuthorizedError()
+
+    // const user = await User.findById(req.currentUser?.userId);
+
+    // if(!user) throw new NotAuthorizedError()
+
+    next()
+} 
